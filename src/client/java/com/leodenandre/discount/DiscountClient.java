1bf9ea45e514863cc1e4797c0ceaa091da0624ac
Leo André <leo.august.andre@gmail.com>
2025-08-26 12:57:13 +0200

    changed mod id

--- src/client/java/com/leodenandre/discount/DiscountClient.java
+++ src/client/java/com/leodenandre/discount/DiscountClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package com.leodenandre.discount;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class DiscountClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/com/leodenandre/discount/Discount.java
+++ src/main/java/com/leodenandre/discount/Discount.java
@@ -1,12 +1,12 @@
-package com.example;
+package com.leodenandre.discount;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class Discount implements ModInitializer {
+	public static final String MOD_ID = "discount";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
